3a55a241d1747f61ac6cd78d2bfd0fe55e3ebfbf
Unlucky <47713540+Unluckyx@users.noreply.github.com>
2022-05-10 05:31:38 +1000

    Add project files.

---
+++ Unofficial.Net.Bot/Commands/BpCommands.cs
@@ -0,0 +1,21 @@
+﻿using UnofficialNetBot;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace UnofficialNetBot.Commands
+{
+    public class BpCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("startgame")]
+        public async Task startGame(string lichessurl)
+        {
+            
+        }
+    }
+}
